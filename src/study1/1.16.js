575128c95e5686ba8273d72c7a3652c66615ce03
권성대 <kwon.sd@kstc.co.kr>
2023-01-12 21:52:30 +0900

    add 1.16

---
+++ src/study1/1.16.js
@@ -0,0 +1,21 @@
+function fast_expt(b, n) {
+    return iter_fast_expt(1, b, n)
+}
+
+function iter_fast_expt(a, b, n) {
+   if ( n==0 ) {
+        return a;
+    }
+
+     if (is_event(n)) {
+         return iter_fast_expt(a, b*b, n/2)
+     }
+
+    return iter_fast_expt(a*b, b, n-1)
+}
+
+function is_event(n){
+    return n%2 == 0;
+}
+
+console.log(fast_expt(2,10))
