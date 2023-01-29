7b3d41787e35b6dad0f75c9edb20cc3bc3bebf9e
권성대 <kwon.sd@kstc.co.kr>
2023-01-29 22:44:49 +0900

    solved 2.17

--- src/study3/2.17.js
+++ src/study3/2.17.js
@@ -1,7 +1,11 @@
-func length(items: Array<Int>) -> Int {
-    var length = 0
-    for item in items {
-        length += 1
-    }
-    return length
+import { tail, list } from 'sicp';
+
+
+function last_pair(paramList) {
+    // console.log(paramList)
+    return tail(paramList) == null ? paramList :last_pair(tail(paramList))
 }
+
+console.assert(last_pair(list(34)).join(',') === Array(34, null).join(','));
+console.assert(last_pair(list(1,2,3,4,5)).join(',') === Array(5, null).join(','));
+
