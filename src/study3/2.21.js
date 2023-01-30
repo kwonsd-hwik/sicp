05f04b11553c357facdd1d46b0687f0b15b366e8
kwonsd-hwik <80740115+kwonsd-hwik@users.noreply.github.com>
2023-01-30 18:07:35 +0900

    solved 2.21

---
+++ src/study3/2.21.js
@@ -0,0 +1,14 @@
+import {head, tail, list, pair, is_null, map} from 'sicp';
+
+function square_list1(items) {
+    return is_null(items)
+            ? null
+        : pair(head(items)*head(items) , square_list1(tail(items)));
+}
+
+function square_list2(items) {
+    return map( x => x *x, items)
+}
+
+console.assert(square_list1(list(1,2,3,4)).join(",") == Array(1,4,9,16,null).join(","))
+console.assert(square_list2(list(1,2,3,4)).join(",") == Array(1,4,9,16,null).join(","))
